417d99ff9ea7df55703990f88c91e0f2104ec302
unknown <m.meshrif77@gmail.com>
2021-06-26 09:13:46 +0200

    add recentlyViewedProducts methods to local

--- lib/helper/localStorageData.dart
+++ lib/helper/localStorageData.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:ecommerce/model/productModel.dart';
+
 import '../model/userModel.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -30,5 +32,36 @@ class LocalStorageData extends GetxController {
   deleteUserData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('userData');
+  }
+
+  setrecentlyViewedProductsData(List<ProductModel> prods) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(
+        'recentlyViewedProductsData', ProductModel.encode(prods));
+  }
+
+  _getrecentlyViewedProductsData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var recentlyViewedProductsData =
+        prefs.getString('recentlyViewedProductsData');
+    return ProductModel.decode(recentlyViewedProductsData);
+  }
+
+  Future<List<ProductModel>> get getrecentlyViewedProducts async {
+    try {
+      List<ProductModel> recentlyViewedProducts =
+          await _getrecentlyViewedProductsData();
+      if (recentlyViewedProducts == null) {
+        return null;
+      }
+      return recentlyViewedProducts;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  deleterecentlyViewedProductsData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('recentlyViewedProductsData');
   }
 }
