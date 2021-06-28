617dc79bee3cf591761a807b2970d5350626da71
unknown <m.meshrif77@gmail.com>
2021-06-28 18:55:39 +0200

    add filter logic to productsView and searchView

--- lib/view/subViews/productsView.dart
+++ lib/view/subViews/productsView.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce/const.dart';
+
+import '../../core/viewModel/filterViewModel.dart';
+import '../../view/widgets/cutomDrawer.dart';
 import '../../core/viewModel/homeViewModel.dart';
 import '../../model/productModel.dart';
 import '../../view/subViews/productDetails/productDetails.dart';
@@ -19,79 +23,95 @@ class ProductsView extends StatelessWidget {
     return GetBuilder<HomeViewModel>(
       builder: (controller) {
         List<ProductModel> products = controller.products;
-        List<ProductModel> filteredProducts = products.where((prod) {
-          if (fromCategoriesView) {
-            return prod.classification['sub-cat'] == prodsTxt &&
-                prod.classification['category'] == catTxt;
-          }
-          return prod.classification['sub-cat'] == prodsTxt;
-        }).toList();
-        return Scaffold(
-          body: filteredProducts.isEmpty
-              ? Center(
-                  child: CustomText(
-                    txt: 'Comming Sooooooooon !',
-                  ),
-                )
-              : Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 33),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          GestureDetector(
-                              onTap: () => Get.back(),
-                              child: Image.asset('assets/shop/back.png')),
-                          Expanded(
+        return GetBuilder<FilterViewModel>(
+          builder: (filterController) {
+            List<ProductModel> filteredProducts = filterController.isFiltered
+                ? filterController.filteredProducts
+                : products.where((prod) {
+                    if (fromCategoriesView) {
+                      return prod.classification['sub-cat'] == prodsTxt &&
+                          prod.classification['category'] == catTxt;
+                    }
+                    return prod.classification['sub-cat'] == prodsTxt;
+                  }).toList();
+            return Scaffold(
+              key: filterController.catFilterKey,
+              endDrawer: CustomDrawer(),
+              body: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 33),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                            onTap: () {
+                              filterController.clearFilters();
+                              Get.back();
+                            },
+                            child: Image.asset('assets/shop/back.png')),
+                        Expanded(
+                          child: Center(
+                            child: CustomText(
+                                txt: prodsTxt,
+                                fSize: 20,
+                                fWeight: FontWeight.bold),
+                          ),
+                        ),
+                        GestureDetector(
+                            onTap: () => filterController.openFilterDrawer(
+                                searchFilter: false,
+                                products: filteredProducts),
+                            child: Image.asset(
+                              'assets/shop/filter.png',
+                              color:
+                                  filterController.isFiltered ? priColor : null,
+                            )),
+                      ],
+                    ),
+                    filteredProducts.isEmpty && !filterController.isFiltered
+                        ? Expanded(
                             child: Center(
                               child: CustomText(
-                                  txt: prodsTxt,
-                                  fSize: 20,
-                                  fWeight: FontWeight.bold),
+                                txt: 'Comming Sooooooooon !',
+                              ),
                             ),
-                          ),
-                          GestureDetector(
-                              onTap: () => null,
-                              child: Image.asset(
-                                'assets/shop/filter.png',
-                                color: null,
-                              )),
-                        ],
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: 120,
-                          child: GridView.builder(
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: filteredProducts.length,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 3),
-                            itemBuilder: (context, x) => GestureDetector(
-                              onTap: () => Get.to(() => ProductDetails(
-                                    prod: filteredProducts[x],
-                                    fromSearchView: fromSearchView,
-                                  )),
-                              child: Card(
-                                child: Center(
-                                  child: CustomColumImgTT(
-                                    imgUrl: filteredProducts[x].imgUrl,
-                                    txt1: filteredProducts[x].prodName,
-                                    txt2: '\$' + filteredProducts[x].price,
+                          )
+                        : Expanded(
+                            child: Container(
+                              height: 120,
+                              child: GridView.builder(
+                                physics: NeverScrollableScrollPhysics(),
+                                itemCount: filteredProducts.length,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 3),
+                                itemBuilder: (context, x) => GestureDetector(
+                                  onTap: () => Get.to(() => ProductDetails(
+                                        prod: filteredProducts[x],
+                                        fromSearchView: fromSearchView,
+                                      )),
+                                  child: Card(
+                                    child: Center(
+                                      child: CustomColumImgTT(
+                                        imgUrl: filteredProducts[x].imgUrl,
+                                        txt1: filteredProducts[x].prodName,
+                                        txt2: '\$' + filteredProducts[x].price,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  ],
                 ),
+              ),
+            );
+          },
         );
       },
     );
