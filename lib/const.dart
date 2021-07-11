9d58dd4fd1d09fe83299c244e2258f8ff1d24c1d
Meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2021-07-11 07:34:34 +0200

    upload profileView and moreView ui

--- lib/const.dart
+++ lib/const.dart
@@ -2,3 +2,71 @@ import 'package:flutter/material.dart';
 
 const priColor = Color.fromRGBO(255, 105, 105, 1);
 const swatchColor = Color.fromRGBO(81, 92, 111, 1);
+List<Map<String, String>> profileComponents = [
+  {
+    'icon': 'assets/profile-more/all-order.png',
+    'txt': 'All My Orders',
+  },
+  {
+    'icon': 'assets/profile-more/pending-shipments.png',
+    'txt': 'Pending Shipments',
+  },
+  {
+    'icon': 'assets/profile-more/pending-payment.png',
+    'txt': 'Pending Payments',
+  },
+  {
+    'icon': 'assets/profile-more/finished.png',
+    'txt': 'Finished Orders',
+  },
+  {
+    'icon': 'assets/profile-more/invite.png',
+    'txt': 'Invite Friends',
+  },
+  {
+    'icon': 'assets/profile-more/support.png',
+    'txt': 'Customer Support',
+  },
+  {
+    'icon': 'assets/profile-more/rate.png',
+    'txt': 'Rate Our App',
+  },
+  {
+    'icon': 'assets/profile-more/suggest.png',
+    'txt': 'Make a Suggestion',
+  },
+];
+List<Map<String, String>> moreComponents = [
+  {
+    'icon': 'assets/profile-more/shipping.png',
+    'txt': 'Shipping Address',
+  },
+  {
+    'icon': 'assets/profile-more/payment.png',
+    'txt': 'Payment Method',
+  },
+  {
+    'icon': 'assets/profile-more/currency.png',
+    'txt': 'Currency',
+  },
+  {
+    'icon': 'assets/profile-more/language.png',
+    'txt': 'Language',
+  },
+  {
+    'icon': 'assets/profile-more/bell.png',
+    'txt': 'Notification Settings',
+  },
+  {
+    'icon': 'assets/profile-more/shield.png',
+    'txt': 'Privacy Policy',
+  },
+  {
+    'icon': 'assets/profile-more/discuss-issue.png',
+    'txt': 'Frequently Asked Questions',
+  },
+  {
+    'icon': 'assets/profile-more/check-form.png',
+    'txt': 'Legal Information',
+  },
+];
