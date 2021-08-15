7c023244ffd78c78b7a89f82ab0b7097fa457ea9
Meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2021-08-15 07:28:02 +0200

    separate shippinggAddressView to two pages

--- lib/view/subViews/moreView/shippingAddressView/shippingAddressView.dart
+++ lib/view/subViews/moreView/shippingAddressView/shippingAddressView.dart
@@ -1,11 +1,11 @@
-import '../../../model/shippingAddressModel.dart';
-import '../../../core/viewModel/moreViewModel.dart';
+import '../../../../view/subViews/moreView/shippingAddressView/AddShippingAddressView.dart';
+import '../../../../model/shippingAddressModel.dart';
+import '../../../../core/viewModel/moreViewModel.dart';
 import 'package:get/get.dart';
-import '../../../const.dart';
-import '../../../view/widgets/customPopMenuButton.dart';
-import '../../../view/widgets/customText.dart';
+import '../../../../const.dart';
+import '../../../widgets/customText.dart';
 import 'package:flutter/material.dart';
-import '../../widgets/customAppBar.dart';
+import '../../../widgets/customAppBar.dart';
 
 class ShippingView extends StatelessWidget {
   @override
@@ -155,185 +155,11 @@ class ShippingView extends StatelessWidget {
                     ],
                   ),
                 )
-              : SingleChildScrollView(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                  child: Form(
-                    key: moreController.shippingKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CustomRowWidget(
-                          title: 'Full Name',
-                          widget: Container(
-                            width: 170,
-                            child: TextFormField(
-                              validator: (val) {
-                                if (val.isEmpty) {
-                                  return 'The Value is Empty';
-                                }
-                                return null;
-                              },
-                              onChanged: (val) => moreController.fullName = val,
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top: 5),
-                                hintText: 'Full Name',
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        CustomRowWidget(
-                          title: 'Mobile Number',
-                          widget: Container(
-                            width: 170,
-                            child: TextFormField(
-                              validator: (val) {
-                                if (val.isEmpty) {
-                                  return 'The Value is Empty';
-                                }
-                                return null;
-                              },
-                              onChanged: (val) =>
-                                  moreController.mobileNumber = val,
-                              keyboardType: TextInputType.phone,
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(top: 5),
-                                hintText: 'Mobile Number',
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        CustomRowWidget(
-                          title: 'State',
-                          widget: Container(
-                            width: 170,
-                            child: CustomPopUpMenuButton(
-                              title: moreController.state,
-                              popList: states,
-                              onSelected: (val) => moreController.getState(val),
-                              enabled: true,
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        CustomRowWidget(
-                          title: 'City',
-                          widget: Container(
-                            width: 170,
-                            child: CustomPopUpMenuButton(
-                              title: moreController.city,
-                              popList: cities[moreController.state],
-                              onSelected: (val) => moreController.getCity(val),
-                              enabled: moreController.state == 'Select State'
-                                  ? false
-                                  : true,
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        SizedBox(height: 10),
-                        CustomText(
-                          txt: 'Street',
-                          fSize: 20,
-                        ),
-                        TextFormField(
-                          validator: (val) {
-                            if (val.isEmpty) {
-                              return 'The Value is Empty';
-                            }
-                            return null;
-                          },
-                          onChanged: (val) => moreController.street = val,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(top: 5),
-                            hintText: 'Street',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                        Divider(),
-                        shippingList.isNotEmpty
-                            ? Column(
-                                children: [
-                                  CustomRowWidget(
-                                    title: 'Set as default',
-                                    widget: Switch.adaptive(
-                                      value: moreController.isDef,
-                                      onChanged: (val) =>
-                                          moreController.getIsDef(val),
-                                      activeColor: priColor,
-                                    ),
-                                  ),
-                                  Divider(),
-                                  SizedBox(height: 20),
-                                ],
-                              )
-                            : SizedBox(height: 20),
-                        Align(
-                          alignment: Alignment(0, 0),
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 80, vertical: 15),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                textStyle: const TextStyle(fontSize: 20),
-                                primary: priColor,
-                              ),
-                              onPressed: () {
-                                moreController.shippingKey.currentState.save();
-                                if (moreController.shippingKey.currentState
-                                    .validate()) {
-                                  moreController.addAddress(
-                                    ShippingAddressModel(
-                                      fullName: moreController.fullName,
-                                      mobileNumber: moreController.mobileNumber,
-                                      state: moreController.state,
-                                      city: moreController.city,
-                                      street: moreController.street,
-                                      isDef: moreController.isDef == true ||
-                                              shippingList.isEmpty
-                                          ? 1
-                                          : 0,
-                                      isSelected: 0,
-                                    ),
-                                  );
-                                }
-                              },
-                              child: CustomText(
-                                txt: 'Add Address',
-                                txtColor: Colors.white,
-                              )),
-                        )
-                      ],
-                    ),
-                  ),
+              : AddShippingAddressView(
+                  moreController: moreController,
                 ),
         );
       },
-    );
-  }
-}
-
-class CustomRowWidget extends StatelessWidget {
-  final String title;
-  final Widget widget;
-
-  CustomRowWidget({@required this.title, @required this.widget});
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        CustomText(
-          txt: title,
-          fSize: 20,
-        ),
-        widget
-      ],
     );
   }
 }
