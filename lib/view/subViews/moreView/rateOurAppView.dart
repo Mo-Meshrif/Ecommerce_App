4fce465cd389193d965502e353eff61be14b3904
Meshrif <meshrif@Meshrifs-MacBook-Pro.local>
2021-09-24 23:05:04 +0200

    upload rateOurAppView ui

--- lib/view/subViews/moreView/rateOurAppView.dart
+++ lib/view/subViews/moreView/rateOurAppView.dart
@@ -1,6 +1,9 @@
+import '../../../view/widgets/customText.dart';
+import '../../../const.dart';
 import '../../../view/widgets/customAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class RateOurAppView extends StatelessWidget {
   @override
@@ -10,6 +13,54 @@ class RateOurAppView extends StatelessWidget {
           title: 'Rate Our App',
           backFun: () => Get.back(),
         ),
-        body: null);
+        body: Padding(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).size.height / 6),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Image.asset(
+                  'assets/more/rate_banner.png',
+                ),
+              ),
+              CustomText(
+                txt: 'Your opinion matters to us !',
+                fSize: 25,
+                fWeight: FontWeight.bold,
+              ),
+              SizedBox(height: 10),
+              CustomText(
+                txt: 'We work super hard to make Glose',
+                fSize: 17,
+                txtColor: Colors.grey[600],
+              ),
+              CustomText(
+                txt: 'better for you,and would love to know :',
+                fSize: 17,
+                txtColor: Colors.grey[600],
+              ),
+              CustomText(
+                txt: 'how would you rate our app ?',
+                fSize: 17,
+                txtColor: Colors.grey[600],
+              ),
+              SizedBox(height: 15),
+              RatingBar.builder(
+                initialRating: 0,
+                minRating: 1,
+                direction: Axis.horizontal,
+                itemCount: 5,
+                itemSize: 40,
+                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: priColor,
+                ),
+                onRatingUpdate: (rating) => null,
+              ),
+              SizedBox(height: 40),
+            ],
+          ),
+        ));
   }
 }
